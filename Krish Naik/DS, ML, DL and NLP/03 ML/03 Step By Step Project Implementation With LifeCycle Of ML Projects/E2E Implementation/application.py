1183e8a4556bdf6a0bf5ad0ac5ca7ab6b6c423b1
ItRachii <awasthirachit7@gmail.com>
2025-05-14 11:50:49 +0530

    Refactor application to use HTML template for the home route

--- Krish Naik/DS, ML, DL and NLP/03 ML/03 Step By Step Project Implementation With LifeCycle Of ML Projects/E2E Implementation/application.py
+++ Krish Naik/DS, ML, DL and NLP/03 ML/03 Step By Step Project Implementation With LifeCycle Of ML Projects/E2E Implementation/application.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -7,9 +7,13 @@ from sklearn.preprocessing import StandardScaler
 application = Flask(__name__)
 app=application
 
+## import ridge regressor and standard scaler pickle file
+ridge_model = pickle.load(open("models/ridge.pkl", "rb"))
+standard_scaler = pickle.load(open("models/scaler.pkl", "rb"))
+
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
