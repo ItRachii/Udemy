c9551ce07cb94cc54b8cf9b23d60be1f2c64d663
ItRachii <awasthirachit7@gmail.com>
2025-05-14 15:32:51 +0530

    Implement prediction functionality and add prediction HTML template for FWI model

--- Krish Naik/DS, ML, DL and NLP/03 ML/03 Step By Step Project Implementation With LifeCycle Of ML Projects/E2E Implementation/application.py
+++ Krish Naik/DS, ML, DL and NLP/03 ML/03 Step By Step Project Implementation With LifeCycle Of ML Projects/E2E Implementation/application.py
@@ -15,5 +15,15 @@ standard_scaler = pickle.load(open("models/scaler.pkl", "rb"))
 def hello_world():
     return render_template("index.html")
 
+@app.route("/predict", methods=["GET", "POST"])
+def predict_datapoint():
+    if request.method == "POST":
+        data = [[float(x) for x in request.form.values()]]
+        final_input = standard_scaler.transform(np.array(data))
+        prediction = ridge_model.predict(final_input)
+        return render_template("predict.html", prediction_text=f"Predicted value: {prediction[0]}")
+    else:
+        return render_template("predict.html")
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
